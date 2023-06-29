34f3cb3cef22ff6cc2d226e84b20bf5c6979f627
Shi Kelong <578622705@qq.com>
2023-06-29 20:19:56 +0800

    (test) add jest and relevant unit tests

---
+++ src/utils/__tests__/tweet.spec.ts
@@ -0,0 +1,42 @@
+import { extractTweetId } from '../tweet';
+
+describe('[extractTweetId]', () => {
+  it.each([
+    ['https://twitter.com/elonmusk/status/1673402963757244420', '1673402963757244420'],
+    ['twitter.com/elonmusk/status/1673402963757244420', '1673402963757244420'],
+    [' https://twitter.com/elonmusk/status/1673402963757244420 ', '1673402963757244420'],
+    ['https://twitter.com/elonmusk/status/1673402963757244420?test=12213', '1673402963757244420'],
+    [
+      'precontentxxxx https://twitter.com/elonmusk/status/1673402963757244420?test=12213 postcontentxxxxx',
+      '1673402963757244420',
+    ],
+    [
+      'precontentxxxx https://twitter.com/elonmusk/status/1673402963757244420?test=12213 xxxxasdasd https://twitter.com/elonmusk/status/16734029637572442120?test=12213 postcontentxxxxx',
+      '1673402963757244420',
+    ],
+    [
+      `precontentxxxx 
+      https://twitter.com/elonmusk/status/1673402963757244420?test=12213 xxxx
+      asdasd https://twitter.com/elonmusk/status/16734029637572442120?test=12213 postcontentxxxxx`,
+      '1673402963757244420',
+    ],
+    // add more test cases here
+  ])('should get tweetId from valid string: %s', (input, expected) => {
+    const tweetId = extractTweetId(input);
+    expect(tweetId).toEqual(expected);
+  });
+
+  it.each([
+    'https://google.com',
+    'https://twitter.com/elonmusk/status/',
+    'https://twitter.com/elonmusk/status/invalidId',
+    'invalid string',
+    undefined,
+    null,
+    '',
+    '12324',
+  ])('should return null for invalid string: %s', (input) => {
+    const tweetId = extractTweetId(input as string);
+    expect(tweetId).toBeNull();
+  });
+});
--- src/utils/tweet.ts
+++ src/utils/tweet.ts
@@ -1,6 +1,9 @@
-export function extractTweetId(tweetUrl: string): string | undefined {
+export function extractTweetId(tweetUrl: string): string | null {
+  if (!tweetUrl) {
+    return null;
+  }
   tweetUrl = tweetUrl.trim();
-  const pattern = /^(https?:\/\/)??twitter\.com\/(?:#!\/)?(\w+|_|\s)+\/status\/(\d+)$/;
+  const pattern = /(https?:\/\/)??twitter\.com\/(?:#!\/)?(\w+|_|\s)+\/status\/(\d+)/i;
   const match = tweetUrl.match(pattern);
-  return match?.at(-1);
+  return match?.at(-1) ?? null;
 }
